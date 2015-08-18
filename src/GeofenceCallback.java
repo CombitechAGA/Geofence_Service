a457b3bcc25c8facee187887a8d4991a5f0bfa2f
Fredrik <freddefolke1@hotmail.com>
2015-08-18 14:46:36 +0200

    fixed changed snapshot message format

--- src/GeofenceCallback.java
+++ src/GeofenceCallback.java
@@ -38,7 +38,7 @@ public class GeofenceCallback implements MqttCallback{
            if(clientToInfo.containsKey(carID)){
                double longitude = Double.parseDouble(message.split("longitude:")[1].split(";")[0]);
                System.out.println(longitude);
-               double latitude = Double.parseDouble(message.split("latitude:")[1]);
+               double latitude = Double.parseDouble(message.split("latitude:")[1].split(";")[0]);
                System.out.println(latitude);
                GeofenceInfo geofenceInfo = clientToInfo.get(carID);
                geofenceInfo.setCurrentLong(longitude);
